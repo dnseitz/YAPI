d142d70c213ca246b1c577267342ba8d6b57e93d
Daniel Seitz <dnseitz@gmail.com>
2016-09-26 14:53:27 -0700

    initial demo commit

---
+++ YAPI/YAPIDemo/SearchRequestViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchRequestViewController.swift
+//  YAPI
+//
+//  Created by Daniel Seitz on 9/12/16.
+//  Copyright © 2016 Daniel Seitz. All rights reserved.
+//
+
+import UIKit
+
+class SearchRequestViewController: UIViewController {
+
+}
---
+++ YAPI/YAPIDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YAPIDemo
+//
+//  Created by Daniel Seitz on 9/26/16.
+//  Copyright © 2016 Daniel Seitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
