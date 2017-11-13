b03410bc1df41e445bcb0208662cc703cb2b492b
Daniel Seitz <dnseitz@gmail.com>
2017-11-12 22:19:28 -0800

    update secret keys and load at runtime from file rather than hardcode

--- project-alpha/project-alpha/ViewController.swift
+++ project-alpha/project-alpha/ViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 import YAPI
 
 class ViewController: UIViewController {
+  
+  private func getKeys() -> [String: String] {
+    guard
+      let path = Bundle.main.path(forResource: "secrets", ofType: "plist"),
+      let keys = NSDictionary(contentsOfFile: path) as? [String: String]
+      else {
+        assertionFailure("Unable to load secrets property list, contact dnseitz@gmail.com if you need the file")
+        return [:]
+    }
+    return keys
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    YelpAPIFactory.V3.authenticate(appId: "", clientSecret: "") { error in
+    let keys = getKeys()
+    guard
+      let appId = keys["APP_ID"],
+      let clientSecret = keys["CLIENT_SECRET"]
+      else {
+        assertionFailure("Unable to retrieve appId or clientSecret from file")
+        return
+    }
+    
+    YelpAPIFactory.V3.authenticate(appId: appId, clientSecret: clientSecret) { error in
       if let error = error {
         print("\(error)")
       }
